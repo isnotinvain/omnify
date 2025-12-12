ff64a947f02d721ae0e2af2f0d87eabd687c0558
Alex Levenson <alex@isnotinvain.com>
2025-12-12 12:16:31 -0800

    formatting fixes

--- vst/ui/components/VariantSelectorItem.cpp
+++ vst/ui/components/VariantSelectorItem.cpp
@@ -96,55 +96,56 @@ void VariantSelectorItem::setSelectedIndex(int index) {
     if (auto* comboBox = getComboBox()) {
         comboBox->setSelectedItemIndex(index, juce::sendNotificationSync);
     }
+}
 
-    void VariantSelectorItem::updateChildVisibility() {
-        auto* comboBox = getComboBox();
-        if (comboBox == nullptr) {
-            return;
-        }
-
-        int selectedIndex = comboBox->getSelectedItemIndex();
-
-        // Find and hide the currently visible item, saving its values
-        for (auto it = begin(); it != end(); ++it) {
-            auto* item = it->get();
-            if (item == comboBoxItem) {
-                continue;
-            }
-
-            if (item->isVisible()) {
-                auto& flexItem = item->getFlexItem();
-                originalMaxSizes[item] = {flexItem.maxWidth, flexItem.maxHeight};
-                flexItem.maxWidth = 0;
-                flexItem.maxHeight = 0;
-                item->setVisible(false);
-            }
-        }
-
-        // Show the selected item
-        if (auto* selectedItem = getVariantChildAtIndex(selectedIndex)) {
-            auto savedIt = originalMaxSizes.find(selectedItem);
-            if (savedIt != originalMaxSizes.end()) {
-                auto& flexItem = selectedItem->getFlexItem();
-                flexItem.maxWidth = savedIt->second.first;
-                flexItem.maxHeight = savedIt->second.second;
-            }
-            selectedItem->setVisible(true);
-        }
-
-        updateLayout();
+void VariantSelectorItem::updateChildVisibility() {
+    auto* comboBox = getComboBox();
+    if (comboBox == nullptr) {
+        return;
     }
 
-    foleys::GuiItem* VariantSelectorItem::getVariantChildAtIndex(int index) {
-        int variantIndex = 0;
-        for (auto it = begin(); it != end(); ++it) {
-            if (it->get() == comboBoxItem) {
-                continue;
-            }
-            if (variantIndex == index) {
-                return it->get();
-            }
-            ++variantIndex;
+    int selectedIndex = comboBox->getSelectedItemIndex();
+
+    // Find and hide the currently visible item, saving its values
+    for (auto it = begin(); it != end(); ++it) {
+        auto* item = it->get();
+        if (item == comboBoxItem) {
+            continue;
         }
-        return nullptr;
+
+        if (item->isVisible()) {
+            auto& flexItem = item->getFlexItem();
+            originalMaxSizes[item] = {flexItem.maxWidth, flexItem.maxHeight};
+            flexItem.maxWidth = 0;
+            flexItem.maxHeight = 0;
+            item->setVisible(false);
+        }
     }
+
+    // Show the selected item
+    if (auto* selectedItem = getVariantChildAtIndex(selectedIndex)) {
+        auto savedIt = originalMaxSizes.find(selectedItem);
+        if (savedIt != originalMaxSizes.end()) {
+            auto& flexItem = selectedItem->getFlexItem();
+            flexItem.maxWidth = savedIt->second.first;
+            flexItem.maxHeight = savedIt->second.second;
+        }
+        selectedItem->setVisible(true);
+    }
+
+    updateLayout();
+}
+
+foleys::GuiItem* VariantSelectorItem::getVariantChildAtIndex(int index) {
+    int variantIndex = 0;
+    for (auto it = begin(); it != end(); ++it) {
+        if (it->get() == comboBoxItem) {
+            continue;
+        }
+        if (variantIndex == index) {
+            return it->get();
+        }
+        ++variantIndex;
+    }
+    return nullptr;
+}
